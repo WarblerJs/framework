1ff7e1a92d1de269f321e17c4e11069f4a221e92
Bellib <bellib@hotmail.fr>
2026-08-06 20:39:00 +0100

    Fix di refa

--- playground/src/graphs/user/presentation/http/user.controller.ts
+++ playground/src/graphs/user/presentation/http/user.controller.ts
@@ -1,5 +1,5 @@
 import { inject } from "@warbler/core";
-import { Controller, Get, Post } from "@warbler/http";
+import { Controller, Get, JsonRes, Post } from "@warbler/http";
 import { CreateUserUseCase } from "../../application/use-cases/create-user.use-case";
 import { FindUserUseCase } from "../../application/use-cases/find-user.use-case";
 import { ListUsersUseCase } from "../../application/use-cases/list-users.use-case";
@@ -12,8 +12,8 @@ export class UserController {
   readonly #listUsers = inject(ListUsersUseCase);
 
   @Get("/")
-  list() {
-    return this.#listUsers.execute(100, 0);
+  async list() {
+    return JsonRes( await this.#listUsers.execute(100, 0));
   }
 
   @Get("/:id")
