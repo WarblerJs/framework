4f4076670b0774ca226e1dbd7192c4cfd8cb2803
Bellib <bellib@hotmail.fr>
2026-08-08 08:08:59 +0100

    test(http,validator,runtinme): add most app tests

--- playground/src/graphs/user/presentation/validators/valiate_user_id.validator.ts
+++ playground/src/graphs/user/presentation/validators/valiate_user_id.validator.ts
@@ -4,4 +4,8 @@ export const valiateUserID = defineValidator({
   paramRules: {
     id: v.uuid('id_invalid_uuid')
   },
+  headerRules: {
+    "x-retries": v.coerce.number("validators.invalid_retries").int("validators.invalid_retries").nonnegative("validators.invalid_retries"),
+  },
+
 });
