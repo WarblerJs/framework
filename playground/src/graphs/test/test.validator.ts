b179c800ce45a897a67471dd4878fb0834887be9
bellib <bellib@hotmail.fr>
2026-08-23 08:34:30 +0000

    test(validators): test some inputs

--- playground/src/graphs/test/test.validator.ts
+++ playground/src/graphs/test/test.validator.ts
@@ -1,11 +1,28 @@
 import { defineValidator, v } from "@warbler/validators";
 
 export const testValidator = defineValidator({
-  csrf: true,
+  csrf: false,
 
   bodyRules: {
     message: v
       .string("validators.invalidMessage")
-      .min(1, "validators.invalidMessage"),
+      .min(1, "validators.invalidMessageLength")
+      .mapK('message_content')
+      .mapV( o => o + ' >>---added')
+      ,
   },
+  paramsRules: {
+    id: v
+      .number('pr_number_req')
+  },
+  queryRules: {
+    id: v
+      .number('qy_number_req')
+  },
+  headerRules: {
+    'x-language': v
+     .string('x_language') 
+     .in(['de','en','fr','ar'],'x_lan_not_allowed')
+     .notIn(['ar'])
+  }
 });
